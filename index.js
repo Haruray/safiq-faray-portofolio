90f26bdf96817c4d24fb3a6815dcf2e9e9aec730
Safiq Faray <syafiqfaray2@gmail.com>
2024-05-28 16:13:21 +0700

    fix index

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
+import $ from "jquery";
+import "popper.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
